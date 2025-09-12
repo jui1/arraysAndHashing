26d9a359e812e6cf662e777759a4096e4de79e1f
Jui Mandal <juimandal@Juis-MacBook-Air.local>
2025-09-13 00:09:35 +0530

    first commit

---
+++ Sum.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Sum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // value -> index
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            
+            map.put(nums[i], i); // store number with its index
+        }
+
+        return new int[] {}; // if no solution
+    }
+}
